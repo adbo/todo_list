d4dd0d2013c6499986d6da5e3e495da77ddc279b
borowskia <adam.borowski@gazeta.pl>
2019-04-22 20:33:32 +0200

    Add classes and reorganize code

--- app.js
+++ app.js
@@ -1,19 +1,81 @@
-const add = () => {
-  const todo_input = document.getElementById("todo_input").value;
-  const li = document.createElement("li");
-  const todo_text = document.createTextNode(todo_input);
-  
-  li.appendChild(todo_text);
-  document.getElementById("todo_list").appendChild(li);
-  document.getElementById("todo_input").value = "";
+class Todo {
+  constructor(name, done=false) {
+    this.name = name;
+    this.done = done;
+  }
 }
+
+class TodoList {
+  constructor(todos=[]) {
+    this.todos = todos;
+  }
+
+  add(todo) {
+    this.todos.push(todo);
+  }
+}
+
+class TodoComponent {
+  constructor(todo) {
+    this.todo = todo;
+  }
+
+  render() {
+    const li = document.createElement("li");
+    const todo_text = document.createTextNode(this.todo.name);
+
+    li.appendChild(todo_text);
+    return li;
+  }
+}
+
+class TodoListComponent {
+  constructor(todoList, todoTag, todoInput) {
+    this.todoList = todoList;
+    this.todoTag = todoTag;
+    this.todoInput = todoInput;
+  }
+
+  addTodo(todo) {
+    const todoComponent = new TodoComponent(todo);
+
+    todoList.add(todo);
+    document.getElementById(this.todoTag).appendChild(todoComponent.render());
+    document.getElementById(this.todoInput).value = "";
+  }
+
+  add() {
+    const name = document.getElementById(this.todoInput).value;
+    const newTodo = new Todo(name);
+
+    this.addTodo(newTodo);
+    this.saveTodos();
+  }
+
+  loadTodos() {
+    const storageTodos = JSON.parse(localStorage.getItem('todos'));
+    
+    if(storageTodos !== null) {
+      storageTodos.map(todo => this.addTodo(todo));
+    }
+  }
+
+  saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todoList.todos));
+  }
+}
+
+const todoList = new TodoList();
+const todoListComponent = new TodoListComponent(todoList, "todo_list", "todo_input");
+
+todoListComponent.loadTodos();
 
 const keyup = event => {
   if(event.key !== "Enter") 
     return false;
-  add();
+  todoListComponent.add();
   event.preventDefault();
 }
 
-document.getElementById("todo_add").onclick = add;
+document.getElementById("todo_add").onclick = todoListComponent.add;
 document.getElementById("todo_input").addEventListener("keyup", event => keyup(event));
