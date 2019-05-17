79bf574bc7cc2abe7616b2bc98c62099cd2c129f
Adam Borowski <adam.borowski@gazeta.pl>
2019-05-17 14:05:50 +0200

    refactor genuuid util

--- src/utils/Uuid.js
+++ src/utils/Uuid.js
@@ -1,22 +1,32 @@
-export default function genUuid() {
+export default genUuid = () => {
   // returns a 36-character string in the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
   // where "X" is an "upper-case" hexadecimal digit [0-9A-F].
 
-  // generate 16 random bytes
-  let randomValues = Array.from({ length: 16 }, () => Math.floor(Math.random() * 0x100));
+  return reduceWithDashes(toHex(setBytes(randomBytes())));
+}
 
-  // set the high nibble of the 7th byte equal to 4 and
-  // set the two most significant bits of the 9th byte to 10'B,
-  // so the high nibble will be one of {8,9,A,B}.
-  randomValues[6] = (randomValues[6] & 0x0f) | 0x40; // eslint-disable-line no-bitwise
-  randomValues[8] = (randomValues[8] & 0x3f) | 0x80; // eslint-disable-line no-bitwise
+// generate 16 random bytes
+const randomBytes = () => {
+  return Array.from({ length: 16 }, () => Math.floor(Math.random() * 0x100));
+}
 
-  // convert to hex strings
-  randomValues = randomValues.map(x => x.toString(16).toUpperCase());
+// set the high nibble of the 7th byte equal to 4 and
+// set the two most significant bits of the 9th byte to 10'B,
+// so the high nibble will be one of {8,9,A,B}.
+const setBytes = (bytes) => {
+  return bytes
+    .map((item, index) => index === 6 ? ((item & 0x0f) | 0x40) : item)
+    .map((item, index) => index === 8 ? ((item & 0x3f) | 0x80) : item);
+}
 
-  // add dashes after 6, 8, 10 and 12 bytes and add '0' in case of 1 character values
-  const uuid = randomValues.reduce((sum, x, index) => sum
+// convert to Hex
+const toHex = (bytes) => {
+  return bytes.map(x => x.toString(16).toUpperCase());
+}
+
+// reduce to single string 
+// add dashes after 6, 8, 10 and 12 bytes and add '0' in case of 1 character values
+const reduceWithDashes = (bytes) => {
+  return randomValues.reduce((sum, x, index) => sum
     + ([6, 8, 10, 12].includes(index) ? '-' : '') + (x.length === 1 ? '0' : '') + x, '');
-
-  return uuid;
 }
